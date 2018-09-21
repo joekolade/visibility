f98bbbe3869e329d8db091b7ad882808380efb28
Joekolade <joe@joekola.de>
2018-09-21 11:07:13 +0200

    [Task] Add gridelement support

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,1 +1,11 @@
 lib.contentElement.layoutRootPaths.200 = EXT:visibility/Resources/Private/Layouts
+
+# Gridelement support
+// Add visibility to bootstrap grids
+lib.gridelements.defaultGridSetup {
+    dataWrap = <div class="visibility" data-xs="{field:visibilityxs}" data-sm="{field:visibilitysm}" data-md="{field:visibilitymd}" data-lg="{field:visibilitylg}">|</div>
+}
+
+page.includeCSS {
+    tx_visibility = EXT:visibility/Resources/Public/Styles/tx_visibility.css
+}
