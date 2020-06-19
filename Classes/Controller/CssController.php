4f7bae79a5eca3a81a1869c4a65173c017c87a26
Joekolade <joe@joekola.de>
2020-06-19 22:06:12 +0200

    [BUGFIX] fallback to no class if no visibility setting is done for a content element

--- Classes/Controller/CssController.php
+++ Classes/Controller/CssController.php
@@ -1,42 +1,39 @@
 <?php
+
 namespace Joekolade\Visibility\Controller;
 
 /***************************************************************
  *  Copyright notice
- *
  *  (c) 2020 Joe Schäfer <joe@joekola.de>, Schäfer - Büro für Webentwicklung
- *
  *  All rights reserved
- *
  *  This script is part of the TYPO3 project. The TYPO3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
- *
  *  The GNU General Public License can be found at
  *  http://www.gnu.org/copyleft/gpl.html.
- *
  *  This script is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
 /**
- *
- *
  * @package visibility
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
- *
  */
-class CssController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+class CssController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
 
   public function renderCssAction()
   {
-    var_dump(49218);
     // todo
+    $this->view->assignMultiple(
+      [
+        'settings' => $this->settings,
+      ]
+    );
   }
 }
