904a5ffbcb20c0ee50b572ba8d8ab15417c818be
Joekolade <joe@joekola.de>
2020-06-19 21:43:51 +0200

    [DEV] make css work with all breakpoints

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -6,6 +6,7 @@ lib.gridelements.defaultGridSetup {
   dataWrap = <div class="visibility" data-xs="{field:visibilityxs}" data-sm="{field:visibilitysm}" data-md="{field:visibilitymd}" data-lg="{field:visibilitylg}">|</div>
 }
 
+
 page.includeCSS {
   tx_visibility = EXT:visibility/Resources/Public/Styles/tx_visibility.css
 }
@@ -27,23 +28,21 @@ visibility_css {
   typeNum = {$plugin.tx_visibility.settings.ajaxpagetype}
 
   config {
-    disableAllHeaderCode = 1
-    xhtml_cleaning = 0
-    admPanel = 0
     additionalHeaders = Content-type: text/css
-    no_cache = 1
   }
 
   10 = USER
   10 {
     userFunc = TYPO3\CMS\Extbase\Core\Bootstrap->run
     extensionName = Visibility
-    pluginName = Pi1
+    pluginName = CssRenderer
     vendorName = Joekolade
     controller = Css
+    action = renderCss
+
     switchableControllerActions {
       Css {
-        1 = renderCssAction
+        1 = renderCss
       }
     }
   }
